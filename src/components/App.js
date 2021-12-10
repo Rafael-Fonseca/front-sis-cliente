fa23c59f5afd82248e34959bb144507528463f00
Rafael-Fonseca <rafael.abreu.unb@gmail.com>
2021-12-10 09:00:42 -0300

    Retirando excessos

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        oi
+      </header>
+    </div>
+  );
+}
+
+export default App;
